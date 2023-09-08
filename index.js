86f9cffd6be5e4aba26272f5b9be70db85672167
Al Amin <alamin931761@gmail.com>
2023-09-09 04:47:38 +0600

    delete print

--- index.js
+++ index.js
@@ -281,6 +281,14 @@ async function run() {
             res.send(result);
         });
 
+        // delete print 
+        app.delete(`/allPrint/:id`, verifyJWT, verifyOwner, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await printCollection.deleteOne(query);
+            res.send(result);
+        });
+
     } finally {
 
     }
